56e8e63dde8f8d633cbe9a4c2ca9e93081c00411
thecode-master-sy <thecodemasster@gmail.com>
2024-09-21 15:19:16 +0100

    feat: anchor landing page

---
+++ app/_components/navbar.tsx
@@ -0,0 +1,135 @@
+import Logo from "@/assets/logo.png";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ChevronsRight, UserPlus } from "lucide-react";
+import Link from "next/link";
+import {
+	Dialog,
+	DialogTrigger,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogDescription,
+	DialogFooter,
+} from "@/components/ui/dialog";
+import tiktok from "@/assets/tiktok-icon.png";
+import whatsapp from "@/assets/whatsapp-icon.png";
+import telegram from "@/assets/telegram-icon.png";
+import { Users } from "lucide-react";
+
+export default function NavBar({
+	isOpen,
+	setIsOpen,
+}: {
+	isOpen: boolean;
+	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+	return (
+		<nav className="flex justify-between items-center">
+			<div className="w-[170px]">
+				<Image src={Logo} alt="logo" className="w-full h-full aspect-[1/0.6]" />
+			</div>
+
+			<div className="hidden md:block">
+				<ul className="flex gap-7 items-center">
+					<li>
+						<a href="#home">Home</a>
+					</li>
+					<li>
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a href="#services">Services</a>
+					</li>
+					<li>
+						<a href="#contact">Contact</a>
+					</li>
+				</ul>
+			</div>
+
+			<Dialog open={isOpen} onOpenChange={setIsOpen}>
+				<DialogTrigger asChild>
+					<Button
+						onClick={() => setIsOpen(true)}
+						size="lg"
+						className="bg-white text-black gap-2 hover:bg-gray-200"
+					>
+						<UserPlus size={16} />
+						<span>Register</span>
+					</Button>
+				</DialogTrigger>
+				<DialogContent className="border-gray-800 bg-black">
+					<DialogHeader className="grid gap-5">
+						<DialogTitle className="text-3xl">Get Registered Now!</DialogTitle>
+						<p>
+							Message our verfied agent on whatsapp for your anchor Registration
+						</p>
+
+						<Button
+							size="lg"
+							className="bg-red-700 hover:bg-red-500 max-w-[200px]"
+							asChild
+						>
+							<a
+								href="https://wa.link/812h7w"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<span>Click here</span>
+								<ChevronsRight />
+							</a>
+						</Button>
+					</DialogHeader>
+					{/* Add your form or content here */}
+					<DialogFooter className="grid">
+						<p>
+							it is also important for you to join our verfied social handles
+							for more information on how anchor works
+						</p>
+
+						<div className="flex gap-x-2 mt-7">
+							<Button
+								asChild
+								variant="ghost"
+								className="bg-[#110C09] h-full hover:bg-gray-900 p-4 border border-gray-500 rounded-lg"
+							>
+								<a
+									href="https://wa.link/812h7w"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<Image
+										src={whatsapp}
+										className="w-[30px] h-[30px]"
+										alt="whatsapp image"
+									/>
+								</a>
+							</Button>
+							<div className="bg-[#110C09] p-4 border border-gray-500 rounded-lg">
+								<Image
+									src={telegram}
+									className="w-[30px] h-[30px]"
+									alt="telegram icon"
+								/>
+							</div>
+							<div className="bg-[#110C09] p-4 border border-gray-500 rounded-lg">
+								<Image
+									src={tiktok}
+									className="w-[30px] h-[30px]"
+									alt="tiktok icon"
+								/>
+							</div>
+							<div className="bg-[#110C09] p-4 border border-gray-500 rounded-lg">
+								<Users />
+							</div>
+						</div>
+
+						<p className="mt-4">
+							Follow our backup account for more info on anchor
+						</p>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</nav>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,390 @@
+"use client";
 import Image from "next/image";
+import NavBar from "./_components/navbar";
+import anchorMe from "@/assets/anchorme.jpg";
+import { Button } from "@/components/ui/button";
+import { ChevronsRight, Clock3 } from "lucide-react";
+import anchorTitktok from "@/assets/anchortiktok.jpg";
+import multinational from "@/assets/multinational.jpg";
+import silverPackage from "@/assets/silver-package.jpg";
+import proPackage from "@/assets/pro-package.jpg";
+import { useState } from "react";
+import cac from "@/assets/cac.jpg";
+import roadmap from "@/assets/roadmap.jpg";
+import { Users } from "lucide-react";
+import tiktok from "@/assets/tiktok-icon.png";
+import whatsapp from "@/assets/whatsapp-icon.png";
+import telegram from "@/assets/telegram-icon.png";
+import Logo from "@/assets/logo.png";
+
+import {
+	Dialog,
+	DialogTrigger,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogDescription,
+	DialogFooter,
+} from "@/components/ui/dialog";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const [selectedPackage, setSelectedPackage] = useState("silver");
+	const [isOpen, setIsOpen] = useState(false);
+	const [regModalOpen, setRegModalOpen] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className="p-4">
+			<NavBar isOpen={regModalOpen} setIsOpen={setRegModalOpen} />
+
+			<div className="rounded-xl overflow-hidden relative">
+				<Image
+					src={anchorMe}
+					alt="anchor-me"
+					className="w-full aspect-[1/0.7]"
+				/>
+				<div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black to-transparent opacity-60" />
+				<Dialog open={isOpen} onOpenChange={setIsOpen}>
+					<DialogTrigger asChild>
+						<Button size="lg" className="absolute bottom-10 left-10">
+							<span>How it works</span>
+							<ChevronsRight />
+						</Button>
+					</DialogTrigger>
+					<DialogContent className="border-gray-800 bg-black">
+						<DialogHeader className="grid gap-4">
+							<DialogTitle className="text-3xl">How It Works</DialogTitle>
+							<DialogDescription className="text-white grid gap-4">
+								<p>
+									Anchor is simply a smart online job/social advert business
+									that pays every week, for doing these 👇🏽👇🏽
+								</p>
+
+								<ol className="list-decimal grid gap-4 px-4">
+									<li>
+										For Engaging on TikTok and Facebook posts to earn ₦1,200
+										every day.
+									</li>
+									<li>Performing Simple social Tasks</li>
+									<li>
+										Working Advert Jobs for Foreign brands to earn every week.
+									</li>
+								</ol>
+								<p>
+									The best part is that Anchor Has Partnered with FaceBook,
+									Instagram and TikTok so that you can Monetise their social
+									media accounts and get paid every week.. You can make as much
+									as ₦60,000 or more every week once you register on Anchor....
+									And all you need for Anchor is your WhatsApp, or any other
+									social media accounts you have... (Referral is Optional 💯)
+								</p>
+							</DialogDescription>
+						</DialogHeader>
+						<DialogFooter>
+							<Button
+								className="w-full bg-white text-black hover:bg-gray-300"
+								onClick={() => setRegModalOpen(true)}
+							>
+								Click here to Register
+							</Button>
+						</DialogFooter>
+					</DialogContent>
+				</Dialog>
+				<p className="absolute bottom-10 right-10 max-w-[45ch] hidden md:block">
+					Anchor is simply a smart online job or social advert business that
+					pays every week for doing some tasks. We are partnered with facebook,
+					instagram and tiktok so that you can monetise their social media
+					accounts and get paid every week.
+				</p>
+			</div>
+
+			<div id="#about" className="mt-32">
+				<h1 className=" text-5xl md:text-7xl font-bold text-orange-200">
+					Who we are?
+				</h1>
+
+				<div className="px-10 py-10 bg-[#110C09] mt-4 rounded-3xl border border-gray-800">
+					<p>
+						<span className="text-red-600">Anchor</span> origninally represents
+						something that brings stability, support and that can be relied
+						upon. Achor is a universal framework that connects globally
+						continously and was set up after series of serial entrepreneurs with
+						experience and expertise came up with a framework taht would help in
+						providing finacial and non financial support and stability. On
+						Anchor, we provide services that would help various users accros the
+						continent in ensuring that their internet and online presence is not
+						take as a waste of time
+					</p>
+
+					<div className="py-4 px-4 rounded-md bg-[#242520] mt-7 max-w-[550px]">
+						<p>On Anchor, our users welfare is always a top poirity.</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="mt-32">
+				<div className="flex justify-center">
+					<Button
+						size="lg"
+						className="gap-2 items-center rounded-full bg-[#110C09] border border-gray-500"
+					>
+						<Clock3 size={16} />
+						<span>waitlist</span>
+					</Button>
+				</div>
+
+				<div className="px-10 py-10 bg-[#110C09] mt-7 rounded-3xl border border-gray-800">
+					<h1 className=" text-5xl md:text-7xl font-bold text-orange-200">
+						Register Now!
+					</h1>
+
+					<p className="mt-7">
+						Be the first to experience the future of connectivity and
+						innovation. By joining our waitlist, you'll gain exclusive access to
+						our latest updates and insights. Click on the button below to get
+						started and be a part of something extraordinary
+					</p>
+
+					<Button
+						onClick={() => setRegModalOpen(true)}
+						size="lg"
+						className="bg-white w-full max-w-64 text-black mt-7 hover:bg-gray-200"
+					>
+						Click here to Register
+					</Button>
+				</div>
+			</div>
+
+			<div className="mt-32">
+				<div className="flex justify-center">
+					<Button
+						size="lg"
+						className="gap-2 items-center rounded-full bg-[#110C09] border border-gray-500"
+					>
+						<Clock3 size={16} />
+						<span>what you would get</span>
+					</Button>
+				</div>
+
+				<div className="mt-7">
+					<Image
+						src={anchorTitktok}
+						alt="anchor tiktok"
+						className="w-full aspect-[1/0.8]"
+					/>
+				</div>
+
+				<div className="mt-7 px-4">
+					<h1 className=" text-3xl md:text-4xl font-bold">
+						Here's how you can earn on Anchor, once you get Registered
+					</h1>
+
+					<ol className="list-decimal mt-7 grid gap-4 px-4">
+						<li>
+							Once you get Registered, you receive an Enrollment Cashback of
+							₦5,000, which you can withdraw anytime.
+						</li>
+
+						<li>
+							You can do the Daily TikTok Social Job to get an extra ₦1,200
+							daily.
+						</li>
+
+						<li>You can play Anchor games and win up to ₦25,000 weekly.</li>
+
+						<li>
+							You can participate in daily Spillover Earnings, daily Anchor
+							Tenacious Rewards, Spin to Win, and Weekly TikTok challenge to win
+							amazing prizes 😍
+						</li>
+					</ol>
+
+					<p className="mt-7">
+						WITHRAWAL IS THREE TIMES A WEEK WITH MINIMUM OF ₦10,000 Now,
+						Registration fee is a One-time payment of ₦6,000.... AND AFTER
+						REGISTRATION, YOU WILL GET ADDED TO THE VIP GROUP WHERE I'LL TEACH
+						YOU HOW TO MAKE ₦60K to ₦80K WEEKLY
+					</p>
+
+					<Button
+						onClick={() => setRegModalOpen(true)}
+						size="lg"
+						className="bg-white w-full max-w-64 text-black mt-7 hover:bg-gray-200"
+					>
+						Click here to Register
+					</Button>
+				</div>
+			</div>
+
+			<div className="mt-32">
+				<div className="flex justify-center">
+					<Button
+						size="lg"
+						className="gap-2 items-center rounded-full bg-[#110C09] border border-gray-500"
+					>
+						<Clock3 size={16} />
+						<span>We are multinational</span>
+					</Button>
+				</div>
+
+				<div className="mt-7">
+					<Image
+						src={multinational}
+						alt="anchor multinational"
+						className="w-full aspect-[1/0.8]"
+					/>
+				</div>
+			</div>
+
+			<div className="mt-32">
+				<div className="flex justify-center">
+					<Button
+						size="lg"
+						className="gap-2 items-center rounded-full bg-[#110C09] border border-gray-500"
+					>
+						<Clock3 size={16} />
+						<span>Earning Graph</span>
+					</Button>
+				</div>
+
+				<div className="flex gap-4 justify-center items-center mt-7">
+					<Button
+						size="lg"
+						className="gap-2 items-center rounded-full bg-[#110C09] border border-gray-500"
+						onClick={() => setSelectedPackage("silver")}
+					>
+						<span>Silver Package</span>
+					</Button>
+
+					<Button
+						size="lg"
+						className="gap-2 items-center rounded-full bg-[#110C09] border border-gray-500"
+						onClick={() => setSelectedPackage("pro")}
+					>
+						<span>Pro Package</span>
+					</Button>
+				</div>
+
+				<div className="mt-7">
+					<Image
+						src={selectedPackage == "silver" ? silverPackage : proPackage}
+						className="w-full aspect-[1/0.7]"
+						alt="package images"
+					/>
+				</div>
+			</div>
+
+			<div className="mt-32">
+				<div className="flex justify-center">
+					<Button
+						size="lg"
+						className="gap-2 items-center rounded-full bg-[#110C09] border border-gray-500"
+					>
+						<Clock3 size={16} />
+						<span>Earning Graph</span>
+					</Button>
+				</div>
+
+				<div className="mt-7">
+					<Image
+						src={cac}
+						className="w-full aspect-[1/0.7]"
+						alt="package images"
+					/>
+				</div>
+
+				<div className="px-5 flex justify-center itmes-center">
+					<div className="py-4 px-4 rounded-md bg-[#242520] mt-7 max-w-[550px]">
+						<p>Anchor is registered and is safe for every user.</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="mt-32">
+				<div className="flex justify-center">
+					<Button
+						size="lg"
+						className="gap-2 items-center rounded-full bg-[#110C09] border border-gray-500"
+					>
+						<Clock3 size={16} />
+						<span>Roadmap to stability and connection</span>
+					</Button>
+				</div>
+
+				<div className="mt-7">
+					<Image
+						src={roadmap}
+						className="w-full aspect-[1/0.7]"
+						alt="roadmap"
+					/>
+				</div>
+
+				<div className="px-5">
+					<div className="py-4 px-4 rounded-md bg-[#242520] mt-7">
+						<p>
+							Anchor is registered and is safe for every user. Our commitment to
+							security ensures that your data is protected with the highest
+							standards. We prioritize transparency and user trust, providing
+							regular updates and insights into our operations. Join us in
+							building a reliable and innovative platform that empowers users
+							across the globe.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="mt-14 flex flex-col justify-center items-center">
+				<Button className="bg-[#110C09]  border border-gray-500 rounded-full justify-center px-4 py-2 min-w-[150px] flex-row gap-x-2 items-center">
+					<Clock3 size={16} />
+					<span className="text-white text-base font-bold">Join Us</span>
+				</Button>
+
+				<div className="flex gap-x-2 mt-7">
+					<Button
+						asChild
+						variant="ghost"
+						className="bg-[#110C09] h-full hover:bg-gray-900 p-4 border border-gray-500 rounded-lg"
+					>
+						<a
+							href="https://wa.link/812h7w"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image
+								src={whatsapp}
+								className="w-[30px] h-[30px]"
+								alt="whatsapp image"
+							/>
+						</a>
+					</Button>
+					<div className="bg-[#110C09] p-4 border border-gray-500 rounded-lg">
+						<Image
+							src={telegram}
+							className="w-[30px] h-[30px]"
+							alt="telegram icon"
+						/>
+					</div>
+					<div className="bg-[#110C09] p-4 border border-gray-500 rounded-lg">
+						<Image
+							src={tiktok}
+							className="w-[30px] h-[30px]"
+							alt="tiktok icon"
+						/>
+					</div>
+					<div className="bg-[#110C09] p-4 border border-gray-500 rounded-lg">
+						<Users />
+					</div>
+				</div>
+			</div>
+
+			<div className="w-[170px] mx-auto">
+				<Image src={Logo} alt="logo" className="w-full h-full aspect-[1/0.6]" />
+			</div>
+
+			<div className="flex items-center justify-center">
+				<p className="text-white text-center text-base">
+					All rights reserved @Anchor 2024
+				</p>
+			</div>
+		</div>
+	);
 }

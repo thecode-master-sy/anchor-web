f5d97d51c1441ffb1c279133f44fc70867c02f06
thecode-master-sy <thecodemasster@gmail.com>
2024-09-26 14:25:02 +0100

    fix: anchor landing page

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -52,106 +52,16 @@ export default function NavBar({
 				</ul>
 			</div>
 
-			<Dialog open={isOpen} onOpenChange={setIsOpen}>
-				<DialogTrigger asChild>
-					<Button
-						onClick={() => setIsOpen(true)}
-						className="bg-white text-black gap-2 hover:bg-gray-200"
-					>
-						<UserPlus size={16} />
-						<span>Register</span>
-					</Button>
-				</DialogTrigger>
-				<DialogContent className="border-gray-800 bg-black">
-					<DialogHeader className="grid gap-5">
-						<DialogTitle className="text-3xl">Get Registered Now!</DialogTitle>
-					</DialogHeader>
-					{/* Add your form or content here */}
-					<form className="grid gap-4">
-						<Input
-							name="firstname"
-							placeholder="enter your firstname"
-							type="text"
-						/>
-						<Input
-							name="lastname"
-							placeholder="enter your lastname"
-							type="text"
-						/>
-						<div className="flex gap-2 items-center rounded-md border border-white p-1">
-							<Input
-								className="border-0 b"
-								name="code"
-								placeholder="enter your register code"
-								type="text"
-							/>
-							<Button asChild>
-								<a
-									href="https://wa.link/812h7w"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									Get code
-								</a>
-							</Button>
-						</div>
-
-						<Button
-							className="bg-red-800 text-white hover:bg-red-700"
-							onClick={() => setIsOpen(false)}
-						>
-							Continue
-						</Button>
-					</form>
-					<DialogFooter className="grid">
-						<p>
-							it is also important for you to join our verfied social handles
-							for more information on how anchor works
-						</p>
-
-						<div className="flex gap-x-2 mt-7">
-							<Button
-								asChild
-								variant="ghost"
-								className="bg-[#110C09] h-full hover:bg-gray-900 p-4 border border-gray-500 rounded-lg"
-							>
-								<a
-									href="https://wa.link/812h7w"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<Image
-										src={whatsapp}
-										className="w-[30px] h-[30px]"
-										alt="whatsapp image"
-									/>
-								</a>
-							</Button>
-							<div className="bg-[#110C09] p-4 border border-gray-500 rounded-lg">
-								<Image
-									src={telegram}
-									className="w-[30px] h-[30px]"
-									alt="telegram icon"
-								/>
-							</div>
-							<div className="bg-[#110C09] p-4 border border-gray-500 rounded-lg">
-								<Image
-									src={tiktok}
-									className="w-[30px] h-[30px]"
-									alt="tiktok icon"
-								/>
-							</div>
-							<div className="bg-[#110C09] p-4 border border-gray-500 rounded-lg">
-								<Users />
-							</div>
-						</div>
-
-						<p className="mt-4">
-							Follow our backup account for more info on anchor
-						</p>
-					</DialogFooter>
-				</DialogContent>
-			</Dialog>
+			<Button
+				onClick={() => setIsOpen(true)}
+				className="bg-white text-black gap-2 hover:bg-gray-200"
+				asChild
+			>
+				<Link href="/register">
+					<UserPlus size={16} />
+					<span>Register</span>
+				</Link>
+			</Button>
 		</nav>
 	);
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,25 +1,266 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Logo from "@/assets/logo.png";
+import Image from "next/image";
+import { useState } from "react";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import FormAction from "./actions";
+import { useFormState } from "react-dom";
+import { stat } from "fs";
 
-export default function Register() {
+export function SelectDemo() {
 	return (
-		<form>
-			<Input name="firstname" placeholder="enter your name" type="text" />
-			<Input name="lastname" placeholder="enter your name" type="text" />
-			<div className="flex gap-2">
-				<Input name="code" placeholder="enter your register code" type="text" />
+		<Select>
+			<SelectTrigger className=" border-gray-600">
+				<SelectValue placeholder="Select a Package" />
+			</SelectTrigger>
+			<SelectContent className="bg-black border-gray-800">
+				<SelectGroup className="text-muted-foreground">
+					<SelectLabel>Packages</SelectLabel>
+					<SelectItem value="pro" className="hover:bg-gray-800">
+						Pro Package
+					</SelectItem>
+					<SelectItem value="silver">Silver Package</SelectItem>
+				</SelectGroup>
+			</SelectContent>
+		</Select>
+	);
+}
+
+function CheckboxDemo() {
+	return (
+		<div className="flex items-center space-x-2 mt-2">
+			<Checkbox id="terms" className="border border-muted-foreground" />
+			<label
+				htmlFor="terms"
+				className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-muted-foreground"
+			>
+				Accept terms and conditions
+			</label>
+		</div>
+	);
+}
+
+const SetpOne = ({
+	setCurrentStep,
+}: {
+	setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+	return (
+		<div className="grid gap-4 w-full max-w-[450px] mx-auto">
+			<FormGroup>
+				<label htmlFor="firstname" className="text-muted-foreground">
+					Firstname
+				</label>
+				<Input
+					name="firstname"
+					type="text"
+					id="firstname"
+					placeholder="Enter firstname"
+					className="border border-gray-600"
+				/>
+			</FormGroup>
+			<FormGroup>
+				<label htmlFor="lastname" className="text-muted-foreground">
+					Lastname
+				</label>
+				<Input
+					name="lastname"
+					type="text"
+					id="lastname"
+					placeholder="Enter lastname"
+					className="border border-gray-600"
+				/>
+			</FormGroup>
+			<FormGroup>
+				<label htmlFor="username" className="text-muted-foreground">
+					Username
+				</label>
+				<Input
+					name="username"
+					type="text"
+					id="username"
+					placeholder="Enter username"
+					className="border border-gray-600"
+				/>
+
+				<Button
+					className="bg-red-800 hover:bg-red-700"
+					onClick={() => setCurrentStep((previousStep) => previousStep + 1)}
+				>
+					Continue
+				</Button>
+			</FormGroup>
+		</div>
+	);
+};
+
+const StepTwo = ({
+	setCurrentStep,
+}: {
+	setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+	return (
+		<div className="grid gap-4 w-full max-w-[450px] mx-auto">
+			<FormGroup>
+				<label htmlFor="phone-number" className="text-muted-foreground">
+					Phone number
+				</label>
+				<Input
+					name="phone-number"
+					type="tel"
+					id="phone-number"
+					placeholder="Enter phone number"
+					className="border border-gray-600"
+				/>
+			</FormGroup>
+			<FormGroup>
+				<label htmlFor="password" className="text-muted-foreground">
+					Password
+				</label>
+				<Input
+					name="password"
+					type="password"
+					id="password"
+					placeholder="Password"
+					className="border border-gray-600"
+				/>
+			</FormGroup>
+			<FormGroup>
+				<label htmlFor="confirm-password" className="text-muted-foreground">
+					Confirm password
+				</label>
+				<Input
+					name="confirm-password"
+					type="password"
+					id="confirm-password"
+					placeholder="Confirm Password"
+					className="border border-gray-600"
+				/>
+				<div className="flex w-full gap-2">
+					<Button
+						className="flex-1"
+						onClick={() => setCurrentStep((previousStep) => previousStep - 1)}
+					>
+						Previous
+					</Button>
+					<Button
+						className="bg-red-800 hover:bg-red-700 flex-1"
+						onClick={() => setCurrentStep((previousStep) => previousStep + 1)}
+					>
+						Continue
+					</Button>
+				</div>
+			</FormGroup>
+		</div>
+	);
+};
+
+const initialFormState = {
+	error: false,
+	message: "",
+};
+
+const StepThree = ({
+	setCurrentStep,
+}: {
+	setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+	const [state, formAction] = useFormState(FormAction, initialFormState);
+	return (
+		<form
+			className="grid gap-4 w-full max-w-[450px] mx-auto"
+			action={formAction}
+		>
+			{state.error && (
+				<p className="text-red-500 text-center">{state.message}</p>
+			)}
+			<FormGroup>
+				<label htmlFor="coupon-code" className="text-muted-foreground">
+					Coupon code
+				</label>
+				<Input
+					name="coupon-code"
+					type="text"
+					id="coupon-code"
+					placeholder="Coupon code"
+					className="border border-gray-600"
+				/>
+			</FormGroup>
+			<SelectDemo />
+			<FormGroup>
+				<label htmlFor="reffered-by" className="text-muted-foreground">
+					Reffered by
+				</label>
+				<Input
+					name="referred-by"
+					type="text"
+					id="reffered-by"
+					placeholder="bellah02"
+					className="border border-gray-600"
+				/>
+			</FormGroup>
+			<CheckboxDemo />
+			<div className="flex w-full gap-2 mt-2">
+				<Button
+					className="flex-1"
+					onClick={() => setCurrentStep((previousStep) => previousStep - 1)}
+				>
+					Previous
+				</Button>
+				<Button type="submit" className="bg-red-800 hover:bg-red-700 flex-1">
+					Continue
+				</Button>
+			</div>
+			<div className="flex justify-center gap-1">
+				<p>Don't have a code?</p>
 				<a
+					className="text-orange-500"
 					href="https://wa.link/812h7w"
 					target="_blank"
 					rel="noopener noreferrer"
-					className="underline"
 				>
-					Get code
+					Get code here
 				</a>
 			</div>
-
-			<Button>Continue</Button>
 		</form>
 	);
+};
+
+export default function Register() {
+	const [currentStep, setCurrentStep] = useState(0);
+	const stepArray = [
+		<SetpOne setCurrentStep={setCurrentStep} />,
+		<StepTwo setCurrentStep={setCurrentStep} />,
+		<StepThree setCurrentStep={setCurrentStep} />,
+	];
+	return (
+		<div className="flex flex-col gap-4 min-h-[100vh] justify-center">
+			<div className="w-[170px] relative h-[50px] mx-auto">
+				<Image
+					src={Logo}
+					alt="logo"
+					className="w-full h-full absolute object-cover"
+				/>
+			</div>
+			<h1 className="text-3xl font-bold text-center text-muted-foreground">
+				Create an Account
+			</h1>
+			{stepArray[currentStep]}
+		</div>
+	);
 }
+
+const FormGroup = ({ children }: { children: React.ReactNode }) => {
+	return <div className="grid gap-2">{children}</div>;
+};
